f87d62f707030a39d836f9854719b47d4f690618
Yuri Brunetto <yuribrunetto@hotmail.com.br>
2016-03-28 20:50:13 -0300

    add bg and mario

--- js/main.js
+++ js/main.js
@@ -2,10 +2,16 @@ var game = new Phaser.Game(800, 600, Phaser.AUTO);
 
 var GameState = {
     preload: function(){
-
+        this.load.image("background", "assets/img/background.jpg");
+        this.load.image("mario", "assets/img/mario.png");
     },
     create: function(){
+        this.background = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, "background");
+        this.background.anchor.setTo(0.5, 0.5);
 
+        this.mario = this.game.add.sprite(this.game.world.centerX, 400, "mario");
+        this.mario.anchor.setTo(0.5, 0.5);
+        this.mario.scale.setTo(0.2);
     },
     update: function(){
 
